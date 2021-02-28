50da132bc0b641eff1c855085c4c3f85ee9ed65d
mdrdani <sudokill3004@gmail.com>
2021-02-28 19:01:55 +0700

    Landing Page PPDB SDB

--- resources/views/auth/layout/head.blade.php
+++ resources/views/auth/layout/head.blade.php
@@ -2,5 +2,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('img/landingpage/LogoSDBBaru.png') }}" type="image/gif" sizes="16x16">
     <title>@yield('title')</title>
 </head>
