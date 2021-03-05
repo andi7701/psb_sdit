c6429c39650186aabb8eb307adee8d077bfe6285
mdrdani <sudokill3004@gmail.com>
2021-03-05 11:22:31 +0700

    modify Register Controller and View Verify Blade

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -69,6 +70,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'role' => 'User',
             'password' => Hash::make($data['password']),
+            'tahun_ajarans' => Carbon::now()->year,
         ]);
 
         $user->assignRole('User');
