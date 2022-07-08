95a1d023540c951ce0b0caeb834c308cb80161e5
rizkyadiryanto14 <adiryantorizky140820@gmail.com>
2022-07-08 10:48:55 +0800

    update bg

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -67,7 +67,7 @@
         </div>
         <div class="col-lg-7 d-none d-lg-block">
             <div id="auth-right">
-                <img src="{{ asset('assets/images/bg/SDB.jpg') }}"  width="100%" height="100%" alt="">
+                <img src="{{ asset('assets/images/bg/sdit.jpg') }}"  width="100%" height="100%" alt="">
             </div>
         </div>
     </div>
